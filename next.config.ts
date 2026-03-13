1c136df7216f4238e822eeed81af7a9862774476
Tony Kim <hyunbin.kim@mail.utoronto.ca>
2026-03-13 18:32:02 -0400

    fwae

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Your Next.js config
+const nextConfig = {
+  output: 'export', // Add this line
+  images: {
+    unoptimized: true, // Necessary if you're using <Image /> tags on GitHub Pages
+  },
 };
 
 export default nextConfig;
