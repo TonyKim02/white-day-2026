b0d9f26794e75822d2108e8187502ebdac96c7d6
Tony Kim <hyunbin.kim@mail.utoronto.ca>
2026-03-13 18:55:09 -0400

    fawe

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: 'export', // Add this line
+  basePath: '/white-day-2026',
   images: {
     unoptimized: true, // Necessary if you're using <Image /> tags on GitHub Pages
   },
